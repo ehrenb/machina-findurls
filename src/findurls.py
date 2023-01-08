3ab5c9e7c28c15f56057c59de4465b9f9aa9cadc
ehrenb <branden.a.ehrenreich@gmail.com>
2023-01-08 15:17:47 -0500

    convert to support neomodel, use new blacklist, and publish to Identifier instead of creating new obj

--- src/findurls.py
+++ src/findurls.py
@@ -1,10 +1,10 @@
+import base64
 import json
 import string
 
 from ioc_finder import find_iocs
 
 from machina.core.worker import Worker
-from machina.core.models.relationships.extracts import Extracts
 
 def _strings(data, min=4):
     result = ""
@@ -19,13 +19,14 @@ def _strings(data, min=4):
         yield result
 
 class FindURLs(Worker):
-    types = ['*']
-    
+
+    types_blacklist = ['url']
+    next_queues = ['Identifier']
+
     def __init__(self, *args, **kwargs):
         super(FindURLs, self).__init__(*args, **kwargs)
 
     def callback(self, data, properties):
-        # self.logger.info(data)
         data = json.loads(data)
 
         # resolve path
@@ -38,14 +39,25 @@ class FindURLs(Worker):
 
         self.logger.info(f"found urls: {urls}")
 
-        # Resolve origin node
-        origin_node = self.graph.get_vertex(data['id'])
+        # get the appropriate OGM class for the object that was analyzed
+        obj_cls = self.resolve_db_node_cls(data['type']) 
+        obj = obj_cls.nodes.get(uid=data['uid'])
 
-        # For each URL, create a node for it
-        # And link it back to the original node
+        # For each URL, resubmit the URL to Identifier and
+        # manually type it as 'url'
         for url in urls:
-            self.logger.info(f"creating url node for: {url}")
-            url_node = self.graph.urls.create(url=url)
-            self.logger.info("creating link")
-            self.graph.create_edge(Extracts, origin_node, url_node)
+            data_encoded = base64.b64encode(url.encode()).decode()
+            body = json.dumps(
+                {
+                    'data': data_encoded,
+                    'origin': {
+                        'ts': data['ts'],
+                        'md5': data['hashes']['md5'],
+                        'uid': data['uid'],
+                        'type': data['type']
+                    },
+                    'type': 'url'
+                }
+            )
+            self.publish_next(body)
 
